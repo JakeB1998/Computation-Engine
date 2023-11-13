267eac1ccfb729a67773614f12f1df06ed311b32
JakeB1998 <49763318+JakeB1998@users.noreply.github.com>
2023-11-13 14:20:42 -0600

    Filled out operation chain classwith implementation.

--- Engine/OperationChain.cs
+++ Engine/OperationChain.cs
@@ -1,5 +1,58 @@
+
+using System.Collections.Generic;
+
 public class OperationChain
 {
-    private
+    private List<Operation> _chain;
+
+    public List<Operation> Chain
+    {
+        get => _chain;
+        set => _chain = value;
+    }
+
+    public OperationChain()
+    {
+        _chain = new List<Operation>(0);
+    }
+
+    public OperationChain(List<Operation> chain)
+    {
+        _chain = chain;
+    }
+
+    public float ComputeChain()
+    {
+        Operation lastItem = null;
+        for(int i = 0; i < _chain.Count; i++)
+        {
+            Operation item = _chain[i];
+            float result = item.Compute();
+
+            if (i + 1 >= _chain.Count)
+                return result;
+
+
+            Chain[i + 1].Num1 = result;
+            
+        }
+        return float.MinValue;
+    }
+
+
+    public override string ToString()
+    {
+        if (_chain != null && _chain.Count > 0)
+        {
+            string o = "[";
+            foreach(Operation operation in _chain)
+            {
+                o += "\n" + operation?.ToString();
+            }
+            o += "]";
+            return o;
+        }
+        return "[]";
+    }
 
 }
